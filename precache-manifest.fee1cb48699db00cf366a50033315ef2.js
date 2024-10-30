f7aa80836eb788318a5d4a8a1c09b5705f6a55ce
Ss Shiva <ssshiva@Shivas-MacBook-Air.local>
2024-10-30 22:04:22 +0530

    Updates

---
+++ precache-manifest.fee1cb48699db00cf366a50033315ef2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6a47fb5a8565c0c3d87ce2fd4221bc",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9887553b125a0ede75c9",
+    "url": "/Portfolio/static/css/main.4b869718.chunk.css"
+  },
+  {
+    "revision": "1b66b57df1ad2881deba",
+    "url": "/Portfolio/static/js/2.d7bfb68d.chunk.js"
+  },
+  {
+    "revision": "6df8d49e07fe72054483",
+    "url": "/Portfolio/static/js/3.8b257cf5.chunk.js"
+  },
+  {
+    "revision": "9887553b125a0ede75c9",
+    "url": "/Portfolio/static/js/main.f2a6c8a4.chunk.js"
+  },
+  {
+    "revision": "14a1d2fbc5a9b39effb9",
+    "url": "/Portfolio/static/js/runtime~main.d45ed0e6.js"
+  },
+  {
+    "revision": "0adb36cd12311df052d45fc5d937a8b5",
+    "url": "/Portfolio/static/media/Resumesvg.0adb36cd.svg"
+  },
+  {
+    "revision": "8058fc605b17532a175da6db95afc663",
+    "url": "/Portfolio/static/media/additionalskills.8058fc60.svg"
+  },
+  {
+    "revision": "194fc8aa381b9f11bdac6b37218799f8",
+    "url": "/Portfolio/static/media/adminfastpjtimage.194fc8aa.svg"
+  },
+  {
+    "revision": "c12bd73b78987607feae69244957900d",
+    "url": "/Portfolio/static/media/bottom_left_optionpage.c12bd73b.svg"
+  },
+  {
+    "revision": "5133a4b69457bb759c5a164af246dbd0",
+    "url": "/Portfolio/static/media/boxone.5133a4b6.svg"
+  },
+  {
+    "revision": "7790ebd942239d54bfcc3aef19cbd1bd",
+    "url": "/Portfolio/static/media/boxtwo.7790ebd9.svg"
+  },
+  {
+    "revision": "a49e7ba08b3bea201f251716329a4320",
+    "url": "/Portfolio/static/media/devpjtimage.a49e7ba0.svg"
+  },
+  {
+    "revision": "a59e86eef184d7d37048d613cc6074cf",
+    "url": "/Portfolio/static/media/ekyc.a59e86ee.svg"
+  },
+  {
+    "revision": "b63926723f1ecd52a1f175d44cc1eda5",
+    "url": "/Portfolio/static/media/header.b6392672.svg"
+  },
+  {
+    "revision": "28d928c1f8dbb7fac522d0401ddb95ef",
+    "url": "/Portfolio/static/media/header786.28d928c1.svg"
+  },
+  {
+    "revision": "85e57f591e1d86618a8f7a0531fe57af",
+    "url": "/Portfolio/static/media/hello.85e57f59.svg"
+  },
+  {
+    "revision": "cbcbf86b2b6d7d802b2296590ba6fff2",
+    "url": "/Portfolio/static/media/mobilefast.cbcbf86b.svg"
+  },
+  {
+    "revision": "4cb8e8ed3f50af293e154db4e7bf41d6",
+    "url": "/Portfolio/static/media/othersskills.4cb8e8ed.svg"
+  },
+  {
+    "revision": "cac6683e67479dcbc693c7929960788f",
+    "url": "/Portfolio/static/media/resume.cac6683e.svg"
+  },
+  {
+    "revision": "f9cae744aed82fe731bf51607864ff6a",
+    "url": "/Portfolio/static/media/technicalskill.f9cae744.svg"
+  },
+  {
+    "revision": "1c44f891a60935f13ddfe9725edb68c0",
+    "url": "/Portfolio/static/media/top_left_optionpage.1c44f891.svg"
+  },
+  {
+    "revision": "8ccbf6d82f3c476c03f702c11aa89d9a",
+    "url": "/Portfolio/static/media/user_image.8ccbf6d8.svg"
+  },
+  {
+    "revision": "79b1c742192e9c9ee1907c9bf5b31a97",
+    "url": "/Portfolio/static/media/user_image_rightside.79b1c742.svg"
+  }
+]);
